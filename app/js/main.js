fb0d0a3b5b86ad61d6bc5da0a7b8c996c266fe00
Alex Kovalenko <arkov@mail.ru>
2021-09-15 12:58:48 +0300

    test

---
+++ app/js/main.js
@@ -0,0 +1,46 @@
+
+let elementChecked = document.querySelectorAll('.heart');
+for (let element of elementChecked) {
+  element.addEventListener('click', () => {
+	element.classList.toggle('heart--active') 
+	});
+}
+
+let btnChecked = document.querySelectorAll('.btn');
+for (let element of btnChecked) {
+	element.addEventListener('click', () => {
+		// if (element.style.display!="none") {
+			element.style.display="none";
+			element.nextElementSibling.style.display="flex";
+		// }
+
+	});
+}
+
+let minusChecked = document.querySelectorAll('.cart__minus');
+for (let element of minusChecked) {
+	element.addEventListener('click', () => {
+		let test = parseInt(element.nextElementSibling.innerHTML.replace('шт',''));
+
+		// console.log(test);
+		if (test == 1){
+			element.closest('.cart').style.display="none";
+			element.parentNode.previousElementSibling.style.display="block";
+		} else {
+			test -=1;
+			element.nextElementSibling.innerHTML = test + "шт";
+		}
+	})
+}
+
+let plusChecked = document.querySelectorAll('.cart__plus');
+for (let element of plusChecked) {
+	element.addEventListener('click', () => {
+		let test = parseInt(element.previousElementSibling.innerHTML.replace('шт',''));
+
+		console.log(test);
+			test +=1;
+			element.previousElementSibling.innerHTML = test + "шт";
+		
+	})
+}
