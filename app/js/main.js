2685bc48328f58779249048d1f23419e2f182896
Alex Kovalenko <arkov@mail.ru>
2021-09-22 17:46:40 +0300

    a-la hover via js

--- app/js/main.js
+++ app/js/main.js
@@ -8,9 +8,21 @@ for (let element of elementChecked) {
 
 let btnChecked = document.querySelectorAll('.btn');
 for (let element of btnChecked) {
+	element.addEventListener('mouseover', () => {
+		element.classList.add('btn--active');
+	});
+	element.addEventListener('mouseleave', () => {
+		element.classList.remove('btn--active');
+	});
 	element.addEventListener('click', () => {
+		// element.style.background="#06174e";
+		
+		setTimeout(() => {
 			element.style.display="none";
 			element.nextElementSibling.style.display="flex";
+		}, 250);
+
+		
 	});
 }
 
@@ -20,12 +32,28 @@ for (let element of minusChecked) {
 		let test = parseInt(element.nextElementSibling.innerHTML.replace('шт',''));
 		if (test == 1){
 			element.closest('.cart').style.display="none";
-			element.parentNode.previousElementSibling.style.display="block";
+			let el = element.parentNode.previousElementSibling;
+			el.style.display="block";
+			// el.style.background="red";
+			el.addEventListener('mouseover', () => {
+				el.classList.add('btn--active');
+			});
+			el.addEventListener('mouseleave', () => {
+				el.classList.remove('btn--active');
+			});
+			// element.addEventListener('mouseover', () => {
+			// 	element.parentNode.previousElementSibling.style.background = "#1A45DB";
+			// })	
+			// element.addEventListener('mouseout', () => {
+			// 	element.parentNode.previousElementSibling.style.background = "#1A45DB";
+			// })
 		} else {
 			test -=1;
 			element.nextElementSibling.innerHTML = test + " шт";
 		}
 	})
+		
+
 }
 
 let plusChecked = document.querySelectorAll('.cart__plus');
